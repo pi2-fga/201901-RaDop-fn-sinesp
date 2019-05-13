dfe5667e782378321c5989d3f9ac465c2aa6cb7e
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-05-13 16:50:46 -0300

    Configuring CI
    Adds tests and pytest config
    
    Update git ignore
    
    Adds travis CI file
    
    Adds deploy file
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Purpose: Continuous deploy on production environment
+#
+# Author: João Pedro Sconetto <sconetto.joao@gmail.com>
+
+curl -sSL https://cli.openfaas.com | sudo sh
+
+faas-cli -f fn-sinesp.yml build
---
+++ fn-sinesp/test/test_handler.py
@@ -0,0 +1,6 @@
+def inc(x):
+    return x + 1
+
+
+def test_inc_success():
+    assert inc(2) == 3
