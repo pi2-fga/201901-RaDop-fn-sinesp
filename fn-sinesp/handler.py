b74da6dc5b6260a58d57dbe21499ed9c61cc780f
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-05-19 20:19:19 -0300

    Evolve fn-sinesp
    Configure tests
    
    Configure deploy
    
    Update and evolve function the verify errors
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- fn-sinesp/handler.py
+++ fn-sinesp/handler.py
@@ -2,44 +2,68 @@ import json
 import warnings
 from sinesp_client import SinespClient
 
+# Ignore warnings due implementation of SinespCliente
 warnings.simplefilter('ignore')
 
+# Define the type of call of the function
 SINESP_CALL = 'sinesp-call'
+
+# Globa SINESP Client variable
+sc = SinespClient()
+
+
+def error_message(message):
+    status_code = 500
+    response = {'status_code':  status_code, 'response_message': message}
+    return response
+
+
+def get_plate_status(plate):
+    try:
+        result = sc.search(plate)
+        return_code = result['return_code']
+        if int(return_code) != 0:
+            return_message = result['return_message']
+            raise Exception(f'Error while trying to get plate status. '
+                            f'Response message from SINESP: {return_message}')
+    except Exception as err:
+        msg = (f'Unknown error while trying to get plate status. Traceback'
+               f': {err}')
+        raise Exception(msg)
+    else:
+        return result
 
 
 def handle(req):
-    """handle a request to the function
+    """
+    Handle calls with car plates to verify the car status within the SINESP
+    database (brazilian car plates)
     Args:
         req (str): request body
     """
     try:
-        sc = SinespClient()
         package = json.loads(req)
+        # TODO: send received package to audit DB
         request_type = package['type']
         request_payload = package['payload']
         plate = request_payload['plate']
 
         if request_type != SINESP_CALL:
-            status_code = 500
             msg = 'Error!! The message header indicates other function call.' \
                   'Verify if the correct service was called.'
-            response = {'status_code': status_code, 'response_message': msg}
-            return json.dumps(response)
         if not plate:
-            status_code = 500
             msg = 'Error trying to get the SINESP plate!' \
                   'Verify service to correct error'
-            response = {'status_code': status_code, 'response_message': msg}
-            return json.dumps(response)
-        result = sc.search(plate)
-    except Exception as e:
-        status_code = 500
+            return json.dumps(error_message(msg))
+        result = get_plate_status(plate)
+    except Exception as err:
         msg = f'Unknown error while trying to execute the SINESP service' \
-              f'Verify service to correct error. Traceback: {e}'
-        response = {'status_code': status_code, 'response_message': msg}
-        return json.dumps(response)
+              f'Verify service to correct error. Traceback: {err}'
+        return json.dumps(error_message(msg))
     else:
         return json.dumps(
-            {'status_code': 200, 'response': result},
-            ensure_ascii=False
-            )
+            {
+                'status_code': 200,
+                'response': result
+            },
+            ensure_ascii=False)
